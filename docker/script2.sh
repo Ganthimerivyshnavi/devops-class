6e735f53671a59f26f2ee2ead31764c5855829b0
Ganthimerivyshnavi <152151221+Ganthimerivyshnavi@users.noreply.github.com>
2023-12-22 23:13:43 +0530

    Delete docker directory

--- docker/script2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "<h1> container name : `hostname` </h1>" > /var/www/html/index.html
-echo "<h1> container IP   : `hostname -I` </h1>" >> /var/www/html/index.html
-echo "<h1> container date : `date` </h1>" >> /var/www/html/index.html
-
-
-service apache2 start && bash
